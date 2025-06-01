7543658da15bff43f800bbd60ebfa52e47f71a7b
igor-cheb <imadmeister@gmail.com>
2025-06-02 01:16:10 +0400

    feat: hw11 update

--- locustfile_quant.py
+++ locustfile_quant.py
@@ -6,7 +6,7 @@ with open("prompts.txt", "r", encoding="utf-8") as f:
     PROMPTS = [line.strip() for line in f if line.strip()]
 
 class VLLMUser(HttpUser):
-    wait_time = between(0.01, 0.05)  # Simulate some think time between requests
+    wait_time = between(0.05, 0.15)  # Simulate some think time between requests
 
     @task
     def complete_text(self):
@@ -15,7 +15,7 @@ class VLLMUser(HttpUser):
 
         headers = {"Content-Type": "application/json"}
         payload = {
-            "model": "microsoft/phi-1_5",  # Ensure this matches your vLLM server
+            "model": "unsloth/gemma-2b-bnb-4bit",  # Ensure this matches your vLLM server
             "prompt": prompt,
             "max_tokens": 10,  # Generate no more than 10 tokens
             "temperature": 0.8
